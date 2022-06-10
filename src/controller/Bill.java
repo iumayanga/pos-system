2b134b87e7fad348b5688b428437d0941d3dc31d
iumayanga <pl.isuru97@gmail.com>
2022-06-10 15:30:02 +0530

    Modify Bill.java file

--- src/controller/Bill.java
+++ src/controller/Bill.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class Bill {
+    public TextArea txtAreaBill;
+    public Button btnPrint;
+
+    public void btnPrint_OnAction() {
+
+    }
 }
